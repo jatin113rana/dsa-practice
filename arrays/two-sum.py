0a958f2ae36d96d576cb825e4c44ad2604387d9a
jatin113rana <139350923+jatin113rana@users.noreply.github.com>
2026-02-01 23:50:42 +0530

    Add array and math solutions, project config
    Added brute force two-sum solution and a palindrome check implementation. Created placeholders for best-time-to-buy-or-sell-stock. Removed reverse-number.py. Added pyproject.toml for project dependencies and configuration.

---
+++ arrays/two-sum.py
@@ -0,0 +1,12 @@
+
+# Brute Force Approach
+# TODO : Optimize using Hash Map
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+         for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j]  == target:
+                    return [i,j]
+         return[]
+            
