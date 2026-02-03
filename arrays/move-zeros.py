79d12e8ce9f9dafd23e90a47de9900f1a957d340
jatin113rana <139350923+jatin113rana@users.noreply.github.com>
2026-02-03 23:41:01 +0530

    Remove commented first-approach code
    Remove the commented-out first approach implementation from arrays/move-zeros.py, leaving the two-pointer Solution class and import intact. Cleans up dead/commented code to reduce clutter; no functional changes to the algorithm.

--- arrays/move-zeros.py
+++ arrays/move-zeros.py
@@ -1,29 +1,4 @@
-# NOTE - First approach
 from typing import List
-# class Solution:
-#     def moveZeroes(self, nums: List[int]) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         nums2 : List[int]=[]
-#         count = 0
-
-#         for i in range(len(nums)):
-#             if(nums[i] != 0):
-#                 nums2.append(nums[i])
-#             else:
-#                 count = count + 1
-
-#         if(count>0):
-#             for i in range(count):
-#                 nums2.append(0)
-        
-
-#         for i in range(len(nums)):
-#             nums[i]= nums2[i]
-
-
-
 # NOTE - Two pointer approach
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
